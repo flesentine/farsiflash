168b1bdb3d6ca090615b22d25f94689edef38a43
flesentine <chrisgmcc@gmail.com>
2026-08-17 16:45:52 -0700

    Add beginner learning path

---
+++ data/path.js
@@ -0,0 +1,77 @@
+window.FARSI_PATH=[
+  {name:"Start Here",cards:[
+    ["سلام","salaam","hello"],["خداحافظ","khodaahafez","goodbye"],["بله","bale","yes"],["آره","aare","yes / yeah"],
+    ["نه","na / noh","no / nine"],["لطفا","lotfan","please"],["ممنون","mamnoon","thank you"],["مرسی","merci","thanks"],
+    ["ببخشید","bebakhshid","excuse me / sorry"],["خوب","khoob","good / well"],["بد","bad","bad"],["باشه","baashe","okay / alright"],
+    ["من","man","I / me"],["تو","to","you (informal)"],["شما","shomaa","you (formal / plural)"],["او","oo","he / she"],
+    ["ما","maa","we / us"],["آنها","aanhaa","they"],["این","een","this"],["آن","aan / oon","that"],
+    ["چه","che","what"],["کی","ki","who / when"],["کجا","kojaa","where"],["چرا","cheraa","why"],["چطور","chetor","how"],
+    ["چند","chand","how many"],["کدام","kodaam","which"],["اینجا","eenjaa","here"],["آنجا","oonjaa","there"],["الان","alaan","now"],
+    ["خیلی","kheili","very / a lot"],["هم","ham","also / too"],["فقط","faghat","only"],["شاید","shaayad","maybe"],
+    ["درست","dorost","right / correct"],["کمک","komak","help"],["اسم","esm","name"],["دوست","doost","friend / like"],["فارسی","faarsi","Persian"]
+  ]},
+  {name:"Numbers & Time",cards:[
+    ["صفر","sefr","zero"],["یک","yek","one"],["دو","do","two"],["سه","se","three"],["چهار","chahaar","four"],["پنج","panj","five"],
+    ["شش","shesh","six"],["هفت","haft","seven"],["هشت","hasht","eight"],["ده","dah","ten"],["یازده","yaazdah","eleven"],
+    ["دوازده","davaazdah","twelve"],["سیزده","sizdah","thirteen"],["چهارده","chahaardah","fourteen"],["پانزده","paanzdah","fifteen"],
+    ["شانزده","shaanzdah","sixteen"],["هفده","hefdah","seventeen"],["هجده","hejdah","eighteen"],["نوزده","noozdah","nineteen"],["بیست","bist","twenty"],
+    ["امروز","emrooz","today"],["فردا","fardaa","tomorrow"],["دیروز","dirooz","yesterday"],["صبح","sobh","morning"],["ظهر","zohr","noon"],
+    ["عصر","asr","afternoon / evening"],["شب","shab","night"],["روز","rooz","day"],["هفته","hafte","week"],["ماه","maah","month"],
+    ["سال","saal","year"],["ساعت","saa'at","hour / time"],["دقیقه","daghighe","minute"],["وقت","vaght","time"]
+  ]},
+  {name:"Family & People",cards:[
+    ["خانواده","khaanevaade","family"],["مادر","maadar","mother"],["پدر","pedar","father"],["مامان","maamaan","mom"],["بابا","baabaa","dad"],
+    ["خواهر","khaahar","sister"],["برادر","baraadar","brother"],["زن","zan","woman / wife"],["مرد","mard","man"],["همسر","hamsar","spouse"],
+    ["شوهر","shohar","husband"],["بچه","bachche","child / kid"],["کودک","koodak","child"],["دختر","dokhtar","girl / daughter"],["پسر","pesar","boy / son"],
+    ["آدم","aadam","person"],["مردم","mardom","people"],["آقا","aaghaa","sir / Mr."],["خانم","khaanom","lady / Ms."],["جوان","javaan","young"],
+    ["پیر","pir","old / elderly"],["اسم","esm","name"],["سن","sen","age"],["دوست","doost","friend"],["همکار","hamkaar","coworker"],["معلم","mo'allem","teacher"],
+    ["دانشجو","daaneshjoo","student"],["دکتر","doktor","doctor"]
+  ]},
+  {name:"Colors & Describing",cards:[
+    ["قرمز","ghermez","red"],["آبی","aabi","blue"],["سبز","sabz","green"],["زرد","zard","yellow"],["نارنجی","naarenji","orange"],
+    ["بنفش","banafshe","purple"],["صورتی","soorati","pink"],["قهوه‌ای","ghahve'i","brown"],["سیاه","siyaah","black"],["سفید","sefid","white"],
+    ["خاکستری","khaakestari","gray"],["بزرگ","bozorg","big"],["کوچک","koochak","small"],["بلند","boland","tall / loud"],["کوتاه","kootaah","short"],
+    ["زیبا","zibaa","beautiful"],["زشت","zesht","ugly"],["جدید","jadid","new"],["قدیمی","ghadimi","old"],["گرم","garm","warm / hot"],
+    ["سرد","sard","cold"],["سریع","sari'","fast"],["آهسته","aaheste","slowly / slow"],["زیاد","ziyaad","a lot / many"],["کم","kam","little / few"],
+    ["آسان","aasaan","easy"],["سخت","sakht","hard / difficult"],["نزدیک","nazdik","near"],["دور","door","far"],["باز","baaz","open"],
+    ["بسته","baste","closed"],["تمیز","tamiz","clean"],["کثیف","kasif","dirty"],["خالی","khaali","empty"],["پر","por","full"]
+  ]},
+  {name:"Home & Everyday",cards:[
+    ["خانه","khoone / khaane","home / house"],["اتاق","otaagh","room"],["در","dar","door / in"],["پنجره","panjare","window"],["میز","miz","table"],
+    ["صندلی","sandali","chair"],["تخت","takht","bed"],["تلفن","telefon","telephone"],["گوشی","gooshi","phone / handset"],["کامپیوتر","kaampiyootar","computer"],
+    ["تلویزیون","televizion","television"],["کتاب","ketaab","book"],["کاغذ","kaaghaz","paper"],["قلم","ghalam","pen"],["مداد","medaad","pencil"],
+    ["کلید","kelid","key"],["کیف","kif","bag"],["لباس","lebaas","clothes"],["کفش","kafsh","shoe"],["پول","pool","money"],
+    ["کارت","kaart","card"],["ماشین","maashin","car"],["خیابان","khiyaabaan","street"],["آب","aab","water"],["غذا","ghazaa","food"],
+    ["نان","naan / noon","bread"],["برنج","berenj","rice"],["گوشت","goosht","meat"],["مرغ","morgh","chicken"],["ماهی","maahi","fish"],
+    ["میوه","mive","fruit"],["سیب","sib","apple"],["موز","moz","banana"],["چای","chaay","tea"],["قهوه","ghahve","coffee"],
+    ["شیر","shir","milk"],["نمک","namak","salt"],["شکر","shekar","sugar"],["صبحانه","sobhaane","breakfast"],["ناهار","nahaar","lunch"],["شام","shaam","dinner"]
+  ]},
+  {name:"Core Verbs",cards:[
+    ["بودن","boodan","to be"],["داشتن","daashtan","to have"],["کردن","kardan","to do"],["رفتن","raftan","to go"],["آمدن","aamadan","to come"],
+    ["دیدن","didan","to see"],["گفتن","goftan","to say"],["شنیدن","shenidan","to hear"],["خوردن","khordan","to eat"],["نوشیدن","nooshidan","to drink"],
+    ["خواستن","khaastan","to want"],["دانستن","daanestan","to know"],["فهمیدن","fahmidan","to understand"],["گرفتن","gereftan","to take / get"],["دادن","daadan","to give"],
+    ["آوردن","aavardan","to bring"],["بردن","bordan","to take / carry"],["خریدن","kharidan","to buy"],["فروختن","forookhtan","to sell"],["خواندن","khaandan","to read"],
+    ["نوشتن","neveshtan","to write"],["خوابیدن","khaabidan","to sleep"],["نشستن","neshastan","to sit"],["ایستادن","istaadan","to stand"],["ماندن","maandan","to stay"],
+    ["رسیدن","residan","to arrive / reach"],["گذاشتن","gozaashtan","to put / leave"],["پرسیدن","porsidan","to ask"],["جواب","javaab","answer"],["شروع","shoroo","start"],
+    ["تمام","tamaam","finished / all"],["یافتن","yaaftan","to find"],["توانستن","tavaanestan","to be able / can"],["زندگی","zendegi","life"],["کار","kaar","work"]
+  ]},
+  {name:"Places & Getting Around",cards:[
+    ["مدرسه","madrese","school"],["دانشگاه","daaneshgaah","university"],["اداره","edaare","office"],["فروشگاه","forooshgaah","store"],["بازار","baazaar","market"],
+    ["رستوران","restoraan","restaurant"],["بیمارستان","bimarestaan","hospital"],["داروخانه","daarookhaane","pharmacy"],["بانک","baank","bank"],["پارک","paark","park"],
+    ["شهر","shahr","city"],["کشور","keshvar","country"],["راه","raah","way / road"],["راست","raast","right / straight"],["چپ","chap","left"],
+    ["بالا","baalaa","up"],["پایین","paayin","down"],["جلو","jelo","front / ahead"],["عقب","aghab","back / behind"],["داخل","daakhel","inside"],
+    ["بیرون","biroon","outside"],["نزدیک","nazdik","near"],["دور","door","far"],["ایستگاه","istgaah","station"],["فرودگاه","foroodgaah","airport"]
+  ]},
+  {name:"Body & Health",cards:[
+    ["سر","sar","head"],["دست","dast","hand"],["پا","paa","foot / leg"],["چشم","cheshm","eye"],["گوش","goosh","ear"],["دهان","dahaan","mouth"],
+    ["دندان","dandaan","tooth"],["مو","moo","hair"],["صورت","soorat","face"],["قلب","ghalb","heart"],["شکم","shekam","stomach / belly"],["بدن","badan","body"],
+    ["درد","dard","pain"],["مریض","mariz","sick"],["سالم","saalem","healthy"],["دارو","daaroo","medicine"],["دکتر","doktor","doctor"],["بیمار","bimaar","patient / sick person"]
+  ]},
+  {name:"Conversation Builders",cards:[
+    ["و","o / va","and"],["یا","yaa","or"],["اما","ammaa","but"],["ولی","vali","but"],["چون","chon","because"],["اگر","agar","if"],
+    ["برای","baraaye","for"],["با","baa","with"],["از","az","from"],["به","be","to"],["قبل","ghabl","before"],["بعد","ba'd","after / then"],
+    ["همیشه","hamishe","always"],["هرگز","hargez","never"],["گاهی","gaahi","sometimes"],["هنوز","hanooz","still / yet"],["دوباره","dobaare","again"],["همه","hame","all / everyone"],
+    ["هر","har","each / every"],["بعضی","ba'zi","some"],["دیگر","digar","other / another"],["بیشتر","bishtar","more"],["کمتر","kamtar","less"],["مثل","mesl","like / such as"],
+    ["مهم","mohem","important"],["ممکن","momken","possible"],["لازم","laazem","necessary"],["واقعا","vaaghean","really"],["تقریبا","taghriban","almost / approximately"]
+  ]}
+];
